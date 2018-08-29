5cf3c83e22a37c1f88bc17f503f43bc04c3fbec1
bass-2000 <bass-2000@yandex.ru>
2018-08-29 14:54:51 +0300

    first junit test + some allure in pom

--- src/test/java/ru/innopolis/stc12/io/EmployeeDataTest.java
+++ src/test/java/ru/innopolis/stc12/io/EmployeeDataTest.java
@@ -1,12 +1,21 @@
 package ru.innopolis.stc12.io;
 
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 
 public class EmployeeDataTest {
 
     @Test
     public void readListFromFile() {
+        Employee employee1 = new Employee("Alex", 28, 98000, Job.ENGENEER);
+        EmployeeData.clearFile();
+        EmployeeData.save(employee1);
+        ArrayList<Employee> list = EmployeeData.readListFromFile();
+        Assert.assertEquals(employee1,list.get(0));
     }
 
     @Test
